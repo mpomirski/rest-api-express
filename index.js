fd018b5f4334a054718192a1dc49fdfa27febe4d
Michał Pomirski <43695467+mpomirski@users.noreply.github.com>
2023-11-12 15:37:04 +0100

    added mongodb connection

--- index.js
+++ index.js
@@ -3,8 +3,23 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+const mongoString = "'mongodb://127.0.0.1:27017/bazy"
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+
+
 app.use(express.json());
 
 app.listen(3000, () => {
-    console.log(`Server Startted at ${3000}`);
+    console.log(`Server Started at ${3000}`);
 });
+
