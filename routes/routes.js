686e45e5351b060963dea9ea1745b0a7a229e57e
Michał Pomirski <43695467+mpomirski@users.noreply.github.com>
2023-11-12 17:01:59 +0100

    test

--- routes/routes.js
+++ routes/routes.js
@@ -6,25 +6,26 @@ module.exports = router;
 
 router.get('/products', async (req, res) => {
     try {
-        if (req.query.sort_by) {
+        if (req.query.sort_by && typeof req.query.sort_by === 'string') {
             const sortQuery = req.query.sort_by;
+            const fieldName = sortQuery.slice(1);
             if (sortQuery[0] === "+") {
-                const data = await Model.find().sort({ sortQuery.slice(1): 1});
+                const data = await Model.find().sort({ fieldName: 1 });
             }
             else if (sortQuery[0] === "-") {
-                const data = await Model.find().sort({ sortQuery.slice(1): - 1 });
+                const data = await Model.find().sort({ fieldName: - 1 });
             }
-res.json(data)
+            res.json(data)
         }
         else {
-    const data = await Model.find();
-    res.json(data)
-}
+            const data = await Model.find();
+            res.json(data)
+        }
 
     }
     catch (error) {
-    res.status(500).json({ message: error.message })
-}
+        res.status(500).json({ message: error.message })
+    }
 })
 
 //Get by ID Method
